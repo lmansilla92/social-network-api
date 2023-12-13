e5596a1517790b79c00e30fd4490c481f0a23f13
Luis Mansilla <l_mansilla92@yahoo.com>
2023-12-13 13:44:24 -0600

    installed moment to format timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,35 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a")
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const thoughtSchema = new Schema(
   {
@@ -11,6 +41,7 @@ const thoughtSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
+        get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a")
     },
     username: {
         type: String,
@@ -21,8 +52,10 @@ const thoughtSchema = new Schema(
   },
   {
     toJSON: {
+      virtuals: true,
       getters: true,
     },
+    id: false,
   }
 );
 
